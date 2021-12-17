7f3cc0437972a2be843ead0bdbeb30fc84b16a1f
Dustin Gritman <gritmand1@hawkmail.newpaltz.edu>
2021-12-17 11:35:49 -0500

    cleaned up functions

--- Flixtras/popup.js
+++ Flixtras/popup.js
@@ -5,15 +5,7 @@ chrome.storage.local.get(["volumeBColor", "volumeTColor"], (result) => {
         volumeTColor.value = result.volumeTColor;
 });
 
-submitColors.addEventListener("click", async () => {
-    setVolumeBackgroundColor(volumeBColor.value);
-    setVolumeTextColor(volumeTColor.value);
+submitSettings.addEventListener("click", async () => {
+    chrome.storage.local.set({ "volumeBColor": volumeBColor.value });
+    chrome.storage.local.set({ "volumeTColor": volumeTColor.value });
 });
-
-function setVolumeBackgroundColor(newColor) {
-    chrome.storage.local.set({ "volumeBColor": newColor });
-}
-
-function setVolumeTextColor(newColor) {
-    chrome.storage.local.set({ "volumeTColor": newColor });
-}
